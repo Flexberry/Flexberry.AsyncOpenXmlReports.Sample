a0bd0ec12577575d839d2111bbdbb8c69e8b172c
Andrey Chibisov <achibisov@skyori.ru>
2023-04-06 13:41:11 +0500

    Building cars list report
    - backend controller
    - template docx
    - frontend button
    - frontend action
    - fix DateTime

---
+++ src/AsyncOpenXmlReportsSample/ODataBackend/ReportsController.cs
@@ -0,0 +1,61 @@
+﻿namespace IIS.AsyncOpenXmlReportsSample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using ICSSoft.STORMNET;
+    using ICSSoft.STORMNET.Business;
+    using Microsoft.AspNetCore.Mvc;
+    using NewPlatform.Flexberry.Reports;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IDataService dataService;
+
+        public ReportsController(IDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+
+        [HttpPost]
+        public IActionResult Build()
+        {
+            var parameters = new Dictionary<string, object>();
+
+            var template = new DocxReport(Path.Combine("Templates", "Cars.docx"));
+
+            var lcs = new LoadingCustomizationStruct(null)
+            {
+                LoadingTypes = new[] { typeof(Car) },
+                View = Car.Views.CarL,
+            };
+
+            var cars = dataService.LoadObjects(lcs).Cast<Car>().ToList();
+
+            var allCarsParameters = new List<Dictionary<string, object>>();
+
+            foreach (var car in cars)
+            {
+                var singleCarParameters = new Dictionary<string, object>();
+
+                singleCarParameters.Add("CarNumber", car.CarNumber);
+                singleCarParameters.Add("CarDate", car.CarDate.ToString("dd.MM.yyyy"));
+                singleCarParameters.Add("CarBody", EnumCaption.GetCaptionFor(car.CarBody));
+
+                allCarsParameters.Add(singleCarParameters);
+            }
+
+            parameters.Add("Car", allCarsParameters);
+
+            template.BuildWithParameters(parameters);
+
+            var stream = new MemoryStream();
+            template.SaveAs(stream);
+            stream.Position = 0;
+            return File(stream, "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml");
+        }
+    }
+}
--- src/ember-app/app/controllers/i-i-s-async-open-xml-reports-sample-car-l.js
+++ src/ember-app/app/controllers/i-i-s-async-open-xml-reports-sample-car-l.js
@@ -1,4 +1,6 @@
 import ListFormController from 'ember-flexberry/controllers/list-form';
+import $ from 'jquery';
+import config from '../config/environment';
 
 export default ListFormController.extend({
   /**
@@ -8,5 +10,33 @@ export default ListFormController.extend({
     @type String
     @default 'i-i-s-async-open-xml-reports-sample-car-e'
    */
-  editFormRoute: 'i-i-s-async-open-xml-reports-sample-car-e'
+  editFormRoute: 'i-i-s-async-open-xml-reports-sample-car-e',
+
+  actions: {
+    PrintCarList() {
+      //let appState = this.get('appState');
+      //appState.loading();
+
+      $.ajax({
+        async: true,
+        cache: false,
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        url: `${config.APP.backendUrls.root}/api/reports`,
+        dataType: 'blob',
+        success(response) {
+          let link = document.createElement('a');
+          link.href = window.URL.createObjectURL(response);
+          link.download = "Car list.docx";
+          link.style.display = 'none';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        },
+        complete() {
+          //appState.reset();
+        }
+      });
+    },
+  }
 });
