e03cf7170b607532528cc38cd87d48307769153c
Andrey Chibisov <achibisov@skyori.ru>
2023-04-12 13:12:59 +0500

    Building a report

---
+++ src/AsyncOpenXmlReportsSample/ODataBackend/CarListReportController.cs
@@ -0,0 +1,127 @@
+﻿namespace IIS.AsyncOpenXmlReportsSample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using ICSSoft.STORMNET;
+    using ICSSoft.STORMNET.Business;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using NewPlatform.Flexberry.Reports;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarListReportController : ControllerBase
+    {
+        /// <summary>
+        /// App configuration.
+        /// </summary>
+        protected IConfiguration config;
+
+        /// <summary>
+        /// Data service.
+        /// </summary>
+        private readonly IDataService dataService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CarListReportController"/> class.
+        /// </summary>
+        /// <param name="configuration">App configuration.</param>
+        /// <param name="dataService">Data service.</param>
+        public CarListReportController(IConfiguration configuration, IDataService dataService)
+        {
+            this.dataService = dataService;
+            this.config = configuration;
+        }
+
+        public const string TemplateName = "CarListTemplate.docx";
+
+        [HttpGet("[action]")]
+        public IActionResult Build()
+        {
+            try
+            {
+                var parameters = new Dictionary<string, object>();
+
+                var template = new DocxReport(this.config["TemplatesPath"] + TemplateName);
+
+                var lcs = new LoadingCustomizationStruct(null)
+                {
+                    LoadingTypes = new[] { typeof(Car) },
+                    View = Car.Views.CarL,
+                };
+
+                var cars = this.dataService.LoadObjects(lcs).Cast<Car>().ToList();
+
+                var allCarsParameters = new List<Dictionary<string, object>>();
+
+                foreach (var car in cars)
+                {
+                    var singleCarParameters = new Dictionary<string, object>();
+
+                    singleCarParameters.Add("CarNumber", car.CarNumber);
+                    singleCarParameters.Add("CarDate", car.CarDate.ToString("dd.MM.yyyy"));
+                    singleCarParameters.Add("CarBody", EnumCaption.GetCaptionFor(car.CarBody));
+
+                    allCarsParameters.Add(singleCarParameters);
+                }
+
+                parameters.Add("Car", allCarsParameters);
+
+                template.BuildWithParameters(parameters);
+
+                var stream = new MemoryStream();
+                template.SaveAs(stream);
+                stream.Position = 0;
+                return this.File(stream, "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml");
+            }
+            catch (Exception ex)
+            {
+                LogService.LogError(ex);
+                return this.StatusCode(500);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult UploadTemplate(IFormFile file)
+        {
+            try
+            {
+                if (file is null)
+                {
+                    return this.StatusCode(400);
+                }
+
+                using (FileStream output = new (this.config["TemplatesPath"] + TemplateName, FileMode.Open))
+                {
+                    file.CopyTo(output);
+                }
+
+                return this.StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                LogService.LogError(ex);
+                return this.StatusCode(500);
+            }
+        }
+
+        [HttpGet("[action]")]
+        public IActionResult DownloadTemplate()
+        {
+            try
+            {
+                var fullPath = this.config["TemplatesPath"] + TemplateName;
+                var stream = new FileStream(fullPath, FileMode.Open);
+                return this.File(stream, "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml");
+            }
+            catch (Exception ex)
+            {
+                LogService.LogError(ex);
+                return this.StatusCode(500);
+            }
+        }
+    }
+}
--- src/ember-app/app/controllers/i-i-s-async-open-xml-reports-sample-car-l.js
+++ src/ember-app/app/controllers/i-i-s-async-open-xml-reports-sample-car-l.js
@@ -16,30 +16,113 @@ export default ListFormController.extend({
   appState: service(),
 
   actions: {
-    CreateCarListReport() {
+    BuildCarListReport() {
+      let authToken = this.get('keycloakSession.token');
       let appState = this.get('appState');
       appState.loading();
 
       $.ajax({
+        headers: {
+          Authorization: `Bearer ${authToken}`
+        },
         async: true,
         cache: false,
         type: 'GET',
-        contentType: 'application/json; charset=utf-8',
-        url: `${config.APP.backendUrls.root}/api/reports`,
+        url: `${config.APP.backendUrls.root}/api/CarListReport/Build`,
         dataType: 'blob',
         success(response) {
           let link = document.createElement('a');
           link.href = window.URL.createObjectURL(response);
-          link.download = "Car list.docx";
+          link.download = "CarListReport.docx";
           link.style.display = 'none';
           document.body.appendChild(link);
           link.click();
           document.body.removeChild(link);
+        },
+        error(response) {
+          console.log(response);
         },
         complete() {
           appState.reset();
         }
       });
     },
+
+    DownloadCarListTemplate() {
+      let authToken = this.get('keycloakSession.token');
+      let appState = this.get('appState');
+      appState.loading();
+
+      $.ajax({
+        headers: {
+          Authorization: `Bearer ${authToken}`
+        },
+        async: true,
+        cache: false,
+        type: 'GET',
+        url: `${config.APP.backendUrls.root}/api/CarListReport/DownloadTemplate`,
+        dataType: 'blob',
+        success(response) {
+          let link = document.createElement('a');
+          link.href = window.URL.createObjectURL(response);
+          link.download = "CarListTemplate.docx";
+          link.style.display = 'none';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        },
+        error(response) {
+          console.log(response);
+        },
+        complete() {
+          appState.reset();
+        }
+      });
+    },
+
+    UploadCarListTemplate() {
+      let input = document.getElementById('file-upload');
+      input.click();
+    },
+
+    SendCarListTemplateToServer() {
+      const inputFile = document.getElementById("file-upload");
+      let file = inputFile.files[0];
+      var formData = new FormData();
+      formData.append("file", file);
+
+      if (!file) {
+        alert('Не выбран файл');
+        return;
+      }
+
+      let authToken = this.get('keycloakSession.token');
+      let appState = this.get('appState');
+      appState.loading();
+
+      $.ajax({
+        headers: {
+          Authorization: `Bearer ${authToken}`
+        },
+        async: true,
+        cache: false,
+        type: 'POST',
+        url: `${config.APP.backendUrls.root}/api/CarListReport/`,
+        dataType: 'blob',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success(response) {
+          alert('Шаблон успешно загружен');
+          inputFile.value='';
+        },
+        error(response) {
+          console.log(response);
+        },
+        complete() {
+          appState.reset();
+        }
+      });
+    }
   }
 });
