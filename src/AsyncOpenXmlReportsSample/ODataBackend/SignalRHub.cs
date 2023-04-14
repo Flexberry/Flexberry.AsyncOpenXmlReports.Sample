9c1a3c3f22d7f9e4b479ea99a5a2aaf0de734e9b
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2023-04-14 16:21:52 +0500

    Измнение функции, обращающейся к юзеру

--- src/AsyncOpenXmlReportsSample/ODataBackend/SignalRHub.cs
+++ src/AsyncOpenXmlReportsSample/ODataBackend/SignalRHub.cs
@@ -1,6 +1,7 @@
 ﻿namespace IIS.AsyncOpenXmlReportsSample
 {
     using System.Collections.Concurrent;
+    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.SignalR;
 
@@ -23,12 +24,7 @@
         /// Подключение пользователя.
         /// </summary>
         /// <param name="userName">Имя пользователя</param>
-        public async Task AddUser(string userName)
-        {
-            await Task.Run(() => AddUserToList(userName));
-        }
-
-        private void AddUserToList(string userName)
+        public void AddUser(string userName)
         {
             var id = Context.ConnectionId;
             if (!users.ContainsKey(id))
@@ -38,22 +34,17 @@
         }
 
         /// <summary>
-        /// Методы вызывается из клиента (ember-фронтенда) SignalR. Выполняет вызов метода на стороне клиента.
-        /// </summary>
-        /// <param name="username">Имя пользователя, для информации.</param>
-        public async Task SendNotifyUserMessage(string username)
-        {
-            await Clients.All.SendAsync("NotifyUser", $"Hi {username}");
-        }
-
-        /// <summary>
         /// Метод вызывается из клиента (ember-фронтенда) SignalR. Выполняет вызов метода на стороне клиента.
         /// </summary>
         /// <param name="username">Имя пользователя.</param>
         /// <param name="message">Сообщение для пользователя.</param>
-        public async Task ContactTheUser(string username, string message)
+        public async Task SendNotifyUserMessage(string username, string message)
         {
-            await Clients.Caller.SendAsync("ContactTheUser", $"{username}! {message}");
+            if (users.Values.Contains(username))
+            {
+                string id = users.Keys.Where(p => p == username).FirstOrDefault();
+                await Clients.User(id).SendAsync("NotifyUser", $"{username}! {message}");
+            }
         }
 
         /// <summary>
