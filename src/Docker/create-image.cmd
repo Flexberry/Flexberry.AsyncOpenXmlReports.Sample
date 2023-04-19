f7bd62ace63f6798b00759bf717643703ce670f1
Churekov Valentin <VChurekov@neoplatform.ru>
2023-04-19 17:40:11 +0500

    feat: keycloak docker init.

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,5 +1,7 @@
 docker build --no-cache -f SQL\Dockerfile.PostgreSql -t asyncopenxmlreportssample/postgre-sql ../SQL
 
+docker build --no-cache -f Dockerfile.Keycloak -t asyncopenxmlreportssample/keycloak ../..
+
 docker build --no-cache -f Dockerfile -t asyncopenxmlreportssample/app ../..
 
 docker build --no-cache -f Dockerfile.Quartz -t asyncopenxmlreportssample/quartz ../..
